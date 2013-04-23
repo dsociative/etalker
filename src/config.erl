3cc1d5c53b573e7588567bb9dde1520983601a86
dsociative <admin@geektech.ru>
2013-04-23 17:47:14 +0400

    config.erl and config.json

---
+++ src/config.erl
@@ -0,0 +1,17 @@
+%% Copyright
+-module(config).
+-author("dsociative").
+
+%% API
+-export([read/0]).
+
+
+read() ->
+  {ok, Data} = file:read_file("config.json"),
+  {Json} = jiffy:decode(Data),
+  {
+    proplists:get_value(<<"gateway_name">>, Json),
+    proplists:get_value(<<"port">>, Json),
+    proplists:get_value(<<"gateway_channel">>, Json),
+    proplists:get_value(<<"performer_channel">>, Json)
+  }.
